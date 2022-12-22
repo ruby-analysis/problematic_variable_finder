ec7a7ff74e70a6fdd4cd45a56b49dde80ed61f15
Mark Burns <markburns@users.noreply.github.com>
2022-12-22 11:34:09 +0000

    update ignore mechanism

--- lib/problematic_variable_finder/problem_finder.rb
+++ lib/problematic_variable_finder/problem_finder.rb
@@ -6,56 +6,75 @@ module ProblematicVariableFinder
     include FsCaching
 
     def find_problems_in_directory(path, remove_paths=[])
-      files = Dir.glob("#{path}/**/*.rb")
-      files = files.map do |f|
-        f.gsub("//", "/")
-      end
-
-      shortened_files = files.map do |f|
-        shortened = f.dup
-        remove_paths.each do |remove_path|
-          shortened = shortened.gsub(remove_path, '')
-        end
-
-        [f, shortened]
-      end
-
-      shortened_files.reject! do |_, shortened|
-        ignored_file_matches.any? do |to_reject|
-          shortened.include?(to_reject)
-        end
-      end
-
-      shortened_files.flat_map do |full, shortened|
-        path, problems = find_file_problems(full, shortened)
-
-        problems.map do |problem|
-          Problem.new(
-            full_path: full,
-            type: problem[:type],
-            filename: path,
-            line_number: problem[:line_number],
-            code: problem[:name].to_s
-          )
-        end
-      end
+      DirectoryProblems.new(path, remove_paths).call
     end
 
-    def ignored_file_matches
-      @ignored_file_matches ||= ProblematicVariableFinder.read_file(File.expand_path('DEFAULT_IGNORED_FILES', __dir__)).split("\n").map(&:strip)
-    end
+    class DirectoryProblems
+      def initialize(path, remove_paths=[])
+        @path = path
+        @remove_paths = remove_paths
+      end
+      attr_reader :path, :remove_paths
 
-    def find_file_problems(full, shortened)
-      full_path = File.expand_path(full)
+      def call
+        relevant_files.flat_map do |full, shortened|
+          path, problems = find_file_problems(full, shortened)
 
-      problems = begin
-                   MainFinder.call(ProblematicVariableFinder.read_file(full_path))
-                 rescue => e
-                   puts "Error parsing #{full_path} #{e}"
-                   []
-                 end
+          problems.map do |problem|
+            Problem.new(
+              full_path: full,
+              type: problem[:type],
+              filename: path,
+              line_number: problem[:line_number],
+              code: problem[:name].to_s
+            )
+          end
+        end
+      end
 
-      [shortened, problems]
+      def relevant_files
+        shortened_files.reject do |_, shortened|
+          ignored_file_matches.any? do |to_reject|
+            shortened.match?(/#{to_reject}/)
+          end
+        end
+      end
+
+      def shortened_files 
+        files.map do |f|
+          shortened = f.dup
+          remove_paths.each do |remove_path|
+            shortened = shortened.gsub(remove_path, '')
+          end
+
+          [f, shortened]
+        end
+      end
+
+      def files
+        files = Dir.glob("#{path}/**/*.rb")
+
+        files.map do |f|
+          f.gsub("//", "/")
+        end
+      end
+
+      def find_file_problems(full, shortened)
+        full_path = File.expand_path(full)
+
+        problems = begin
+                     MainFinder.call(ProblematicVariableFinder.read_file(full_path))
+                   rescue => e
+                     puts "Error parsing #{full_path} #{e}"
+                     []
+                   end
+
+        [shortened, problems]
+      end
+
+      def ignored_file_matches
+        @ignored_file_matches ||= ProblematicVariableFinder.read_file(File.expand_path('DEFAULT_IGNORED_FILES', __dir__)).split("\n").map(&:strip)
+      end
     end
   end
 end
